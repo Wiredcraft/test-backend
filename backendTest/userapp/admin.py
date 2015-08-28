c69b2eb9f562f402130443fc50d3ddbc3f5465eb
CCharlieLi <ccharlieli@live.com>
2015-08-28 15:54:32 +0800

    init

--- backendTest/userapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from userapp.models import *
-
-
-# Register models 
-admin.site.register(Appuser)
--- backendTest/userapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from userapp.models import *
-
-class AppuserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Appuser
-        fields = ('id','Name','Dob','Address','Description','Created_at')
