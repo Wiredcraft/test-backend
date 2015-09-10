09a8c6f1fccf483531ccd0b6d312662666a16e9b
xavierzhou <xiayezhou@gmail.com>
2015-09-10 21:05:50 +0800

    add test code for subsequential API request after successful login.

--- test/test-login.js
+++ test/test-login.js
@@ -1,6 +1,18 @@
+'use strict'
 var request = require('supertest');
 var app = require('../index.js');
 
+function retrieveCookie(cookies) {
+  var regex = /connect\.sid=(.*);/;
+  var match;
+  for (var i = 0, len = cookies.length; i < len; i++) {
+    match = regex.exec(cookies[i]);
+    if (match) {
+      return match[1];
+    }
+  }
+
+}
 describe('POST /api/login', function() {
   it('should return 400 given invalid body', function(done) {
     request(app)
@@ -29,5 +41,20 @@ describe('POST /api/login', function() {
       .send({username: 'tommy', password: 'anything'})
       .expect(401)
       .end(done);
+  });
+  it('should be able to access the resource after successful login', function(done) {
+    request(app)
+      .post('/api/login')
+      .send({username: 'alice', password: 'secret'})
+      .expect(200)
+      .end(function(err, res) {
+        if (err) { return done(err); }
+        var cookie = retrieveCookie(res.header['Set-Cookie'] || res.header['set-cookie']);
+        request(app)
+          .get('/index')
+          .set('Cookie', 'connect.sid=' + cookie)
+          .expect(200)
+          .end(done);
+      });
   });
 });
