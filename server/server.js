31cb97e0ed4152723c1adde206b065fef7273c1a
sylvainv <sylvain.verly@gmail.com>
2016-09-27 21:55:59 +0800

    Remove annoying console.log messages

--- server/server.js
+++ server/server.js
@@ -8,13 +8,11 @@ var app = module.exports = loopback();
 app.start = function() {
   // start the web server
   return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+    app.get('url').replace(/\/$/, '');
     if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      app.get('loopback-component-explorer').mountPath;
     }
+    app.emit('started');
   });
 };
 
