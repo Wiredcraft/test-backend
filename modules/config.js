656a49605c41bb0d29a2b2977f129b85d85c637e
Xavier <xavier.vanderhoeven@gmail.com>
2016-12-18 18:32:22 +0100

    feat: made config more usable with nconf

--- modules/config.js
+++ modules/config.js
@@ -1,12 +1,20 @@
-module.exports = {
-    server: {
-        port: 8080
-    },
-    db: {
-        host: '127.0.0.1',
-        port: 27017,
-        name: 'test-backend',
-        test: 'test-backend-test'
-    },
-    dateFormat: 'YYYY-MM-DD[T]HH:mm:ss Z',
-};
+const
+nconf = require('nconf');
+
+nconf
+    .argv()
+    .env({ whitelist: [
+        'server:port',
+        'db:host',
+        'db:port',
+        'db:name',
+        'db:test',
+        'db:login:user',
+        'db:login:pass',
+        'dateFormat'
+    ] })
+    .file('./config.json');
+
+const config = nconf.get();
+
+module.exports = config;
