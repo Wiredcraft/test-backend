2cddac86742ee24a4d695e02d31b86a215460994
alutun <alutun@technochina.cn>
2017-07-04 15:57:48 +0800

    Fix syntax issue using eslint, update package json with eslint package in devDependencies

--- server/boot/dataBaseAutoUpdate.js
+++ server/boot/dataBaseAutoUpdate.js
@@ -2,7 +2,7 @@
 * @Author: alutun
 * @Date:   2017-07-03 09:44:37
 * @Last Modified by:   alutun
-* @Last Modified time: 2017-07-03 10:00:27
+* @Last Modified time: 2017-07-04 15:15:18
 */
 
 'use strict';
@@ -12,19 +12,14 @@ var path = require('path');
 var app = require(path.resolve(__dirname, '../server'));
 var ds = app.datasources.postGreSql;
 
-ds.isActual(null, function(err, actual) // using NULL to apply to all the models
-{
-	console.log("Checking Database Structure.");
-  	if (!actual) 
-  	{
-  		console.log("Database Schema Structure is different.")
-    	ds.autoupdate(null, function(err, result) // using NULL to apply to all the model
-    	{
-      		console.log("Database Schema Structure is now up to date.")
+ds.isActual(null, function(err, actual) {
+  console.log('Checking Database Structure.');
+  	if (!actual) {
+  		console.log('Database Schema Structure is different.');
+    	ds.autoupdate(null, function(err, result) {  // using NULL to apply to all the model
+      		console.log('Database Schema Structure is now up to date.');
     	});
-  	}
-  	else
-  	{
-  		console.log("Database Structure is matching !");
+  	} else {
+  		console.log('Database Structure is matching !');
   	}
 });
--- server/models/user.js
+++ server/models/user.js
@@ -1,22 +1,20 @@
 'use strict';
 
 module.exports = function(user) {
+  var methodWhiteList = [
+    'create', 'findById', 'deleteById',
+  ];
 
-let methodWhiteList = [
-  "create", "findById", "deleteById"
-]
-
-user.sharedClass.methods().forEach(function(method) 
-{
-  if (methodWhiteList.indexOf(method.name) == -1) // check if method is in the white list
-  {
+  user.sharedClass.methods().forEach(function(method) {
+    if (methodWhiteList.indexOf(method.name) == -1) { // check if method is in the white list
     	user.disableRemoteMethodByName(method.name); // Method is disable because not white-listed
-  }});
+    }
+  });
 
-// DATA Validation
-// Make sure name is unique and respect length
- user.validatesUniquenessOf('name', {message: 'User Already exist'});
- user.validatesLengthOf('name', {min: 3, max: 42 , message : {
- 	min : 'Name is too short', 
- 	max : 'Name is too long'}});
+  // DATA Validation
+  // Make sure name is unique and respect length
+  user.validatesUniquenessOf('name', {message: 'User Already exist'});
+  user.validatesLengthOf('name', {min: 3, max: 42, message: {
+ 	min: 'Name is too short',
+ 	max: 'Name is too long'}});
 };
