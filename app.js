bed4bd2e98061eff3ba5ae6f55fc1029c2705828
Biga Gaber <ahmedgabr292@gmail.com>
2017-12-23 18:15:36 +0200

    remove useless require

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express')
 const app = express()
 const bodyParser  = require('body-parser')
 const morgan  = require('morgan')
-const mongoose  = require('mongoose')
 const fs  = require('fs')
 const jwt = require('jsonwebtoken')
 
