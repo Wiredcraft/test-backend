1041c95526846f3e329dacc5e041ad7cae2fdcb5
Biga Gaber <ahmedgabr292@gmail.com>
2017-12-25 02:56:54 +0200

    fix eslint issues

--- test/user.js
+++ test/user.js
@@ -3,67 +3,65 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app');
 
-const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Users', () => {
   beforeEach((done) => {
-    User.remove({}, (err) => {
-       done();
+    User.remove({}, () => {
+      done();
     });
   });
 
   describe('/POST users', () => {
     it('it should POST a new user', (done) => {
-      let user = {
+      const user = {
         name: 'ahmed',
         password: '123456',
         description: 'software geek',
         dob: '1992/3/16',
-        address: 'ALexandria, Egypt'
-      }
+        address: 'ALexandria, Egypt',
+      };
       chai.request(server)
-          .post('/api/v1/users')
-          .send(user)
-          .end((err, res) => {
-            res.should.have.status(201);
-            res.body.should.be.a('object');
-            res.body.password.should.be.eql('xxxxxxxxxxxx');
-            done();
-          });
+        .post('/api/v1/users')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.body.password.should.be.eql('xxxxxxxxxxxx');
+          done();
+        });
     });
 
     it('it should not POST a new user if name not in body', (done) => {
-      let user = {
+      const user = {
         password: '123456',
         description: 'software geek',
         dob: '1992/3/16',
-        address: 'ALexandria, Egypt'
-      }
+        address: 'ALexandria, Egypt',
+      };
       chai.request(server)
-          .post('/api/v1/users')
-          .send(user)
-          .end((err, res) => {
-            res.should.have.status(400);
-            done();
-          });
+        .post('/api/v1/users')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
     });
 
     it('it should not POST a new user if password not in body', (done) => {
-      let user = {
+      const user = {
         name: 'biga',
         description: 'software geek',
         dob: '1992/3/16',
-        address: 'ALexandria, Egypt'
-      }
+        address: 'ALexandria, Egypt',
+      };
       chai.request(server)
-          .post('/api/v1/users')
-          .send(user)
-          .end((err, res) => {
-            res.should.have.status(400);
-            done();
-          });
+        .post('/api/v1/users')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
     });
   });
-
 });
