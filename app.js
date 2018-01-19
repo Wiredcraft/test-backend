2a25cf43eeebd2eb1174b7a9625fd017980cfb83
Osman Jalloh <mudu.jalloh@gn.ehealthafrica.org>
2018-01-19 12:23:36 +0000

    feat: setup the server.

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+/* API routes */
+var users = require('./server/routes/users');
+
+var app = express();
+
+/*
+ * uncomment for setting up server side view rendering
+ */
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'server/views'));
+// app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1/', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
