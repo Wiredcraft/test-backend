1d6ccaa51453270b3babfab0d3981a00f9b5ee96
xbeco <pivstone@gmail.com>
2018-01-24 10:52:00 +0800

    Add field Add method

--- api/models/User.js
+++ api/models/User.js
@@ -4,6 +4,8 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+var bcrypt = require('bcrypt');
+
 
 module.exports = {
 
@@ -11,7 +13,8 @@ module.exports = {
     name: {
       type: 'string',
       size: 24,
-      required: true
+      required: true,
+      unique: true
     },
     address: {
       type: 'string',
@@ -24,6 +27,42 @@ module.exports = {
     dob: {
       type: 'date',
       defaultsTo: '1900-01-01'
+    },
+    password: {
+      type: 'string',
+      required: true,
+      columnName: 'hashed_password'
+    },
+
+    isAdmin: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+    toJSON: function() {
+      var obj = this.toObject();
+      delete obj.password;
+      return obj;
     }
+  },
+  beforeCreate: function(value, cb) {
+    bcrypt.hash(value.password, 8, function(err, hash) {
+      if(err) return cb(err);
+      value.password = hash;
+      cb();
+    });
+  },
+
+  authenticate: function (name, password, cb) {
+    User.findOne({name: name}, function(err, user){
+      if(err) return cb(err);
+      if(!user) return cb(null, false, {message: 'name not found'});
+      bcrypt.compare(password, user.password, function(err, res){
+        if(!res) return cb(null, false, { message: 'Invalid Password' });
+        let userDetails = {
+          name: user.name,
+          id: user.id};
+        return cb(null, userDetails, { message: 'Login Succesful'});
+      });
+    });
   }
 };
