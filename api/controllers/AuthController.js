3a62ddd47fc4537a623cff5d0ecc7288e096963c
xbeco <pivstone@gmail.com>
2018-01-24 10:51:21 +0800

    Add authentication controller

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,38 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing auths
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	login: function(req, res) {
+    User.authenticate(req.param('name'),req.param('password'), function(err, user, info){
+      if((err) || (!user)) {
+        return res.unauthorized({
+          message: info.message,
+          user
+        });
+      }
+			req.session.userId = user.id;
+			req.session.authenticated = true;
+      return res.send({
+        message: info.message,
+        user
+      });
+    });
+  },
+	signup: function(req, res){
+		User.create({
+			name: req.param('name'),
+			password: req.param('password')
+		}).exec(function(err, newUser){
+			if(err) {
+				if (!_.isArray(err.invalidAttributes.password)) {
+              return res.badRequest(err);
+        }
+			}
+			return res.send(newUser)
+		});
+	}
+}
