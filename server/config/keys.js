950a2e2aabff5fc6aa77f450b63ee74feae29400
Osman Jalloh <mudu.jalloh@gn.ehealthafrica.org>
2018-01-27 07:26:59 +0000

    fix: move secret keys.

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+/* Keep secret keys here */
+module.exports = {
+  mlab: 'mongodb://mudu:12345@ds263137.mlab.com:63137/wiredcraft',
+  google: {
+    clientID: '422588195480-45722ta56h90vvdocbc1hun51nj5a1vl.apps.googleusercontent.com',
+    clientSecret: 'x81hgB2DTq7aBkEXv1PtcF1C'
+  }
+}
