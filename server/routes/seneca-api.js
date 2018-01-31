1dfa624bb30cf398078068ddfd1314d17ea7bca1
Osman Jalloh <mudu.jalloh@gn.ehealthafrica.org>
2018-01-31 13:17:00 +0000

    fix: trying to configure microservices with seneca.

--- server/routes/seneca-api.js
+++ server/routes/seneca-api.js
@@ -1,16 +1,25 @@
-/* import the employee model */
+/* imports */
+const express = require('express')
 const Employee = require('../models/employee')
 
 /* export the seneca api */
 module.exports = function (options) {
+  // add the router to the plugin
+  this.router = express.Router()
   /* register get pattern */
   this.add('role:api,path:employees', (msg, done) => {
-    this.act('role:employees', {
-      cmd: 'getEmployees'
-    }, (err, res) => {
-      Employee.find((err, employees) => {
-        if (err) {done(err)}
-        done(employees)
+    this.router.get('/api/seneca/employees', (req, res, next) => {
+      this.act('role:employees', {
+        cmd: 'getEmployees'
+      }, (err, response) => {
+        if (err) {return next(err)}
+        Employee.find((err, employees) => {
+          if (err) {
+            res.send(err)
+          }
+          // everything good, return employees
+          res.json(employees)
+        })
       })
     })
   })
@@ -18,7 +27,7 @@ module.exports = function (options) {
   /* register the initialization pattern */
   this.add('init:api', (msg, respond) => {
     this.act('role:web', {routes: {
-      prefix: '/api/v1',
+      prefix: '/api/seneca',
       pin: 'role:api,path:*',
       map: {
         employees: { GET:true }
