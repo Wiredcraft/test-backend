345e051d20a7dfcb61e537c4109f7f45d306d233
Carlos Avilan <avilancj@gmail.com>
2018-02-01 11:51:56 -0400

    Show stack when not in test environment

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -23,6 +23,6 @@ module.exports = {
   addTrace: (err, req, res, next) => // eslint-disable-line no-unused-vars
     res.status(err.status).json({
       message: err.isPublic ? err.message : httpStatus[err.status],
-      stack: config.env === 'development' ? err.stack : {}
+      stack: config.env !== 'test' ? err.stack : {}
     }),
 };
