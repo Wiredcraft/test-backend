af639890e3a2eb516b5bc769e87e6695885c7678
Carlos Avilan <avilancj@gmail.com>
2018-02-01 13:44:56 -0400

    Add authentication middleware

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -1,6 +1,8 @@
 const expressValidation = require('express-validation');
 const httpStatus = require('http-status');
+const jwt = require('jsonwebtoken');
 
+const User = require('../models/user');
 const config = require('../config');
 const APIError = require('../helpers/APIError');
 
@@ -25,4 +27,40 @@ module.exports = {
       message: err.isPublic ? err.message : httpStatus[err.status],
       stack: config.env !== 'test' ? err.stack : {}
     }),
+  authenticate: (req, res, next) => { // eslint-disable-line consistent-return
+    if (!req.headers.authorization || (req.headers.authorization && req.headers.authorization.indexOf('Bearer') === -1)) {
+      return next(new APIError(
+        'Failed to authenticate token',
+        httpStatus.UNAUTHORIZED
+      ));
+    }
+    const token = req.headers.authorization.replace('Bearer ', '');
+    if (token) {
+      jwt.verify(token, config.jwtSecret, (err, decoded) => { // eslint-disable-line
+        if (err) {
+          return next(new APIError(
+            'Failed to authenticate token',
+            httpStatus.UNAUTHORIZED
+          ));
+        }
+        User.findOne({ username: decoded.username })
+          .then((user) => { // eslint-disable-line consistent-return
+            if (!user) {
+              return next(new APIError(
+                'User not Found',
+                httpStatus.NOT_FOUND
+              ));
+            }
+            req.user = user;
+            next();
+          })
+          .catch(next);
+      });
+    } else {
+      return next(new APIError(
+        'Failed to authenticate token',
+        httpStatus.UNAUTHORIZED
+      ));
+    }
+  }
 };
