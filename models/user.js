27ab5fadabd484d3f5ee849bc1f958d800e10a24
Carlos Avilan <avilancj@gmail.com>
2018-02-01 13:43:35 -0400

    Add username and password fields

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const security = require('../helpers/security');
+const config = require('../config');
 
 const Schema = mongoose.Schema; // eslint-disable-line prefer-destructuring
 
@@ -18,14 +20,34 @@ const UserSchema = new Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  username: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
 });
 
+UserSchema.methods = {
+  authenticate(password) {
+    return security.sha512(password, config.pwdSecret) === this.password;
+  },
+};
+
+UserSchema.pre('save', function (next) { // eslint-disable-line
+  if (!this.isModified('password')) return next();
+
+  this.password = security.sha512(this.password, config.pwdSecret);
+  next();
+});
 
 UserSchema.options.toJSON = {
   transform: (doc, ret) => {
     ret.id = ret._id; // eslint-disable-line no-param-reassign
     delete ret._id; // eslint-disable-line no-param-reassign
+    delete ret.password; // eslint-disable-line no-param-reassign
     delete ret.__v; // eslint-disable-line no-param-reassign
     return ret;
   }
