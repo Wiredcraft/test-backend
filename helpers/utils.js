e6f4aa23a6842add78dc8821040e78d2aaab9aed
Carlos Avilan <avilancj@gmail.com>
2018-02-01 13:44:40 -0400

    Validate if user in param is different than user in decoded token

--- helpers/utils.js
+++ helpers/utils.js
@@ -6,11 +6,17 @@ const validObjectID = new RegExp('^[0-9a-fA-F]{24}$');
 
 module.exports = {
   isObjectID: objectID => validObjectID.test(objectID),
-  isValidObjectID: (userId, next) => { // eslint-disable-line consistent-return
+  isValidObjectID: (userId, user, next) => { // eslint-disable-line consistent-return
     if (!validObjectID.test(userId)) {
       return next(new APIError(
         'Not Found',
         httpStatus.NOT_FOUND
+      ));
+    }
+    if (userId !== user.id) {
+      return next(new APIError(
+        'Failed to authenticate token',
+        httpStatus.UNAUTHORIZED
       ));
     }
   }
