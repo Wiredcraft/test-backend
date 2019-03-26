a7fb3365e428e1db8fa5a8aae956c5e8509e78c2
justice <jakattack2020@gmail.com>
2019-03-26 16:46:02 +0800

    dockerize the applicationn and added a beginning for the unit tests

---
+++ tests/unit/service.http.js
@@ -0,0 +1,22 @@
+const app = require('../../src/services/http/handler')
+const request = require('supertest')
+const { expect } = require('chai')
+
+let server
+beforeEach(async () => {
+  server = app.context
+})
+
+afterEach(async () => {
+  server = null
+})
+
+describe('Http Layer Test', async () => {
+  it('Http layer is running successfully', async () => {
+    return request(server)
+      .get('/')
+      .then((res) => {
+        expect(res.body).to.have.property('msg')
+      })
+  })
+})
