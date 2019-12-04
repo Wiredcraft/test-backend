ec8a0a14ee17c1d0a2d7fc5d39044c8ac59d9e9c
CurtiePi <curtisbklyn@gmail.com>
2019-12-04 16:07:56 +0800

    corrected a typo and general formatting

--- config.js
+++ config.js
@@ -2,37 +2,33 @@ require('dotenv').config();
 
 const env = process.env.NODE_ENV;  // 'dev' or 'test'
 
-/*
- * Development environment information
- */
-
-const dev = {
-    app: {
-        port: parseInt(process.env.DEV_APP_PORT) || 3000
-    },
-
-    db: {
-        host: process.env.DEV_DB_HOST || 'localhost',
-        port: parseInt(process.env.DEV_DB_PORT) || 27017,
-        name: process.env.DEV_DB_NAME || 'dev_db' 
-    }
+const dev  = {
+     app: {
+         port: parseInt(process.env.DEV_APP_PORT) || 3000
+     },
+     db: {
+         host: process.env.DEV_DB_HOST || 'localhost',
+         port: parseInt(process.env.DEV_DB_PORT) || 27017,
+         name: process.env.DEV_DB_NAME || 'wired_db'
+     }
 };
 
 const test = {
-    app: {
-        port: parseInt(process.env.TEST_APP_PORT) || 3000
-    },
+     app: {
+         port: parseInt(process.env.TEST_APP_PORT) || 3000
+     },
+     db: {
+         host: process.env.TEST_DB_HOST || 'localhost',
+         port: parseInt(process.env.TEST_DB_PORT) || 27107,
+         name: process.env.TEST_DB_NAME || 'wired_test'
+     }
+ };
 
-    db: {
-        host: process.env.TEST_DB_HOST || 'localhost',
-        port: parseInt(process.env.TEST_DB_PORT) || 27017,
-        name: process.env.TEST_DB_NAME || 'test_db' 
-    }
-};
 
 const config = {
-    dev,
-    test
+  dev,
+  test
 };
 
-module.exports = config(env);
+module.exports = config[env];
+
