91d450cdbb2f785aab833e78c8396ec53a3f181f
CurtiePi <curtisbklyn@gmail.com>
2019-12-06 14:38:19 +0800

    Tesing for the routing

--- test/route_test.js
+++ test/route_test.js
@@ -6,13 +6,15 @@ const app = require('../btapp');
 describe('User API routing testing', () => {
 
     describe('Access user API via /GET requests', () => {
-
+        it("TEST: Get all user records", (done) => {
+            chai.request(app)
+                .get('/list')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
 
     });
-
-    describe('Access user API via /POST requests', () => {
-
-
-    });
-
 });
