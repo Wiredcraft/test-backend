8c257671f2d1d9ad2b0a3015f3087084f6a294c5
CurtiePi <curtisbklyn@gmail.com>
2019-12-07 05:57:28 +0800

    Added functionality to both web and api routing

--- controllers/routing_controller.js
+++ controllers/routing_controller.js
@@ -1,18 +1,19 @@
 let mongoose = require('mongoose');
 let dataAccess = require('./data_access_controller');
+let responseController = require('./response_controller');
+const regex = /^\/api\//;
 
 module.exports = { 
-    getUsersList: async function(req, res, next) {
+    getUsersList: async (req, res, next) =>  {
         try {
             let users = await dataAccess.listUsers()
-        
-            if (users) {
-                return res.json(users);
+            
+            if (regex.exec(req.originalUrl) !== null) {
+                responseController.respondToApiRequest(users, res, next);
+            } else {
+                let data = {'users': users}
+                responseController.respondToWebRequest(data, 'userlist', res, next);
             }
-
-            return res.status(404).json({
-                message: "User list not found"
-            });
         } catch(err) {
            return res.status(500).json({
                message: "Error retrieving user list!" + err
@@ -20,19 +21,48 @@ module.exports = {
         }
     },
 
-    retrieveUser: async function(req, res, next) {
+    retrieveUser: async (req, res, next) => {
         try{
             let user = await dataAccess.getUserById(req.params.userId);
 
-            if(user) {
-                return res.json(user);
+            if (regex.exec(req.originalUrl) !== null) {
+                responseController.respondToApiRequest(user, res, next);
+            } else {
+                if (user) {
+                    let data = {'user': user}
+                    responseController.respondToWebRequest(data, 'user_detail', res, next);
+                } else {
+                    let route = '/list';
+                    responseController.responseWithRedirect(route, res, next);
+                }
             }
-            return res.status(404).json({
-                message: "User specified not found"
-            });
+        } catch(err) {
+               return res.status(500).json({
+               message: "Error retrieving specified user!" + err
+           });
+        }
+    },
+
+    enrollUser: async (req, res, next) => {
+        try {
+            console.log(req.body);
+            let user = await dataAccess.addNewUser(req.body);
+        
+            if (regex.exec(req.originalUrl) !== null) {
+                responseController.respondToApiRequest(user, res, next);
+            } else {
+                if (user) {
+                    let data = {'user': user}
+                    responseController.respondToWebRequest(data, 'user_detail', res, next);
+                } else {
+
+                    let route = '/list';
+                    responseController.responseWithRedirect(route, res, next);
+                }
+            }
         } catch(err) {
            return res.status(500).json({
-               message: "Error retrieving specified user!" + err
+               message: "Error enrolling new user!" + err
            });
         }
     }
