61b3d09fd30a871aa31f737a02d0e65aa734de25
CurtiePi <curtisbklyn@gmail.com>
2019-12-06 12:23:15 +0800

    setting up testing file for the routing

---
+++ test/route_test.js
@@ -0,0 +1,18 @@
+// Test the routing for the application
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../btapp');
+
+describe('User API routing testing', () => {
+
+    describe('Access user API via /GET requests', () => {
+
+
+    });
+
+    describe('Access user API via /POST requests', () => {
+
+
+    });
+
+});
