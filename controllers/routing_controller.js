df1ad8f3ee6e00c883edf5c529cafe54ea4e2cbd
CurtiePi <curtisbklyn@gmail.com>
2019-12-06 17:11:15 +0800

    Allow to get a single user, return JSON response

--- controllers/routing_controller.js
+++ controllers/routing_controller.js
@@ -7,7 +7,7 @@ module.exports = {
             let users = await dataAccess.listUsers()
         
             if (users) {
-                return res.end(JSON.stringify(users));
+                return res.json(users);
             }
 
             return res.status(404).json({
@@ -17,7 +17,23 @@ module.exports = {
            return res.status(500).json({
                message: "Error retrieving user list!" + err
            });
+        }
+    },
 
+    retrieveUser: async function(req, res, next) {
+        try{
+            let user = await dataAccess.getUserById(req.params.userId);
+
+            if(user) {
+                return res.json(user);
+            }
+            return res.status(404).json({
+                message: "User specified not found"
+            });
+        } catch(err) {
+           return res.status(500).json({
+               message: "Error retrieving specified user!" + err
+           });
         }
     }
 }
