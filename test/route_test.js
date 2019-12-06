3d3ba0b5aee9ecb73d2aeb83aee6a5061a4b4598
CurtiePi <curtisbklyn@gmail.com>
2019-12-06 17:08:07 +0800

    added test for finding a single user

--- test/route_test.js
+++ test/route_test.js
@@ -3,6 +3,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../btapp');
 
+const expect = chai.expect;
 // Configure chai
 chai.use(chaiHttp);
 chai.should();
@@ -10,12 +11,23 @@ chai.should();
 describe('User API routing testing', () => {
 
     describe('Access user API via /GET requests', () => {
-        it("TEST: Get all user records", (done) => {
+        it('TEST: Get all user records', (done) => {
             chai.request(app)
                 .get('/list')
                 .end((err, res) => {
                     res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
+                });
+        });
+
+        it('TEST: GET one user by their id', (done) => {
+            chai.request(app)
+                .get('/user/5de88258976347576c2a965d')
+                .end((err, res) => {
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    expect(res.body).to.have.property('name', 'Commander Data');
                     done();
                 });
         });
