e8219862434b80dd2a1c97ad5770644a79763c39
CurtiePi <curtisbklyn@gmail.com>
2019-12-09 11:30:58 +0800

    More javascript support for webpage

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,29 @@
 loadProfile = (rowObj) => {
-    let fields = ["pname", "page", "pdob", "paddress", "pdescription", "pdate"];
-    const it = fields[Symbol.iterator]();
 
     for (const child of rowObj.children) {
-        let domObjName = it.next().value;
-        console.log(domObjName);
-        document.getElementById(domObjName).innerText = child.innerText;
-        //alert("Cell Value is " + child.innerText);
+        let cell_name = child.getAttribute("name");
+        if (cell_name !== null) {
+            let profile_name = 'p' + cell_name;
+            document.getElementById(profile_name).innerText = child.innerText;
+        }
     }
-        document.getElementById("profile_card").style.visibility = "visible";
+    document.getElementById("profile_card").style.visibility = "visible";
 }
+
+loadEditor = (someObj) => {
+    let rowObj =  someObj.parentElement.parentElement;
+   
+    for (const child of rowObj.children) {
+        let cell_name = child.getAttribute("name");
+        if (document.getElementById(cell_name)) {
+            if (cell_name !== null || cell_name !== "age") {
+                document.getElementById(cell_name).value = child.innerText;
+            }
+        }
+    }
+    
+    document.getElementById("sub-btn").value = "Update User";
+}
+
+
+
