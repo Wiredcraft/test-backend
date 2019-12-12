5d1480f58704f473baf3a9799dbde061ccf64ff4
CurtiePi <curtisbklyn@gmail.com>
2019-12-13 06:19:37 +0800

    Small data generator function to load tables with data

--- libs/dataGen.js
+++ libs/dataGen.js
@@ -1,30 +1,44 @@
 const log               = require('./log')(module);
 const mongoose          = require('mongoose');
-const Users             = require('../models/users');
+const Person            = require('../models/person');
 const userData          = require('./user_data');
+const Client            = require('../models/client');
 
+let conn_str = 'mongodb://localhost/wired_backend_dev';
+mongoose.connect(conn_str, {useNewUrlParser: true,
+                            useUnifiedTopology: true,
+                            useCreateIndex: true,
+                            useFindAndModify: false,
+                            keepAlive: true});
 
-mongoose.connect('mongodb://localhost/wired_backend_dev');
 var db = mongoose.connection;
 
 db.on('error', function (err) {
     log.error('connection error:', err.message);
 });
+
 db.once('open', function callback () {
     log.info("Connected to DB!");
 });
 
-Users.deleteMany({}, function(err) {
+Person.deleteMany({}, function(err) {
 
     for(const datum of userData ) {
-        var users = new Users( datum );
-        user.save(function(err, user) {
+        var person = new Person( datum );
+        person.save(function(err, person) {
             if(err) return log.error(err);
-            else log.info("New user - %s:%s",user.name,user.address);
+            else log.info("New Person - %s:%s",person.name,person.address);
         });
     }
 });
 
+Client.remove({}, function(err) {
+    var client = new Client({ name: "OurService iOS client v1", clientId: "mobileV1", clientSecret:"abc123456" });
+    client.save(function(err, client) {
+        if(err) return log.error(err);
+        else log.info("New client - %s:%s",client.clientId,client.clientSecret);
+    });
+});
 
 setTimeout(function() {
     mongoose.disconnect();
