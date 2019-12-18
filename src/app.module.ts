42570ccb04b23c341c46a85acf0bf60f6760a20a
GuanglongDu <duguanglong002@163.com>
2019-12-18 14:43:50 +0800

    remove mongo DeprecationWarning

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SchemaModule } from './service/schema.module';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/test-backend', { useNewUrlParser: true }),
+    MongooseModule.forRoot('mongodb://localhost:27017/test-backend', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true  }),
     SchemaModule],
 })
 
