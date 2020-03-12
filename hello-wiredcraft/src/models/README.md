a55b5948caf16d52ad18e9afbe1953eb87016fd7
Tonghdj <450570469@qq.com>
2020-03-12 19:02:16 +0800

    add user model

--- hello-wiredcraft/src/models/README.md
+++ hello-wiredcraft/src/models/README.md
@@ -1,3 +1,12 @@
 # Models
 
-This directory contains code for models provided by this app.
+## User
+
+| Field | Type | Required |Remark |
+| :----| :---- | :----: | :---- |
+| id          | string  | Y |User ID |
+| name        | string  | Y |User name |
+| dob         | date    | N |Date of birth |
+| address     | string  | N |User address |
+| description | string  | N |User description |
+| createdAt   | date    | Y |User created date |
---
+++ hello-wiredcraft/src/models/user.model.ts
@@ -0,0 +1,56 @@
+import {Entity, model, property} from '@loopback/repository';
+
+// TODO(tong): it need to confirm which field is required.
+// and update the README later.
+@model({settings: {strict: false}})
+export class User extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'date',
+  })
+  dob?: string;
+
+  @property({
+    type: 'string',
+  })
+  address?: string;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+  @property({
+    type: 'date',
+  })
+  createdAt: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
