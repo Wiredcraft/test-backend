fe0aa92c576d8f2f5c3e999dd66915deff4305dd
Quinton <quinton.xu@gmail.com>
2020-03-25 21:49:44 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+class AppBootHook {
+    constructor(app) {
+      this.app = app;
+    }
+  
+    configWillLoad() {
+      // The config file has been read and merged, but it has not yet taken effect
+      // This is the last time the application layer modifies the configuration
+      // Note: This function only supports synchronous calls.
+  
+      // as early as possible require
+      require('appoptics-apm');
+    }
+  }
+  
+  module.exports = AppBootHook;
