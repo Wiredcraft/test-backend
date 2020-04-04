e9c91988451f6c3af7c87881666f3010b1ba29c9
Michael Sayapin <michael@smartmeal.cn>
2020-04-04 17:20:24 +0800

    basic Swagger-based docs for users API

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,7 @@
 
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 from rest_framework.routers import DefaultRouter
 
 from users.views import UserdataViewSet
@@ -9,4 +11,13 @@ router.register('users', UserdataViewSet, basename='user')
 
 urlpatterns = [
 	path('', include(router.urls)),
+	path('schema/', get_schema_view(
+		title='Wiredcraft Backend Test',
+		description='API for managing of user data',
+		version='1.0.0',
+	), name='openapi-schema'),
+	path('docs/', TemplateView.as_view(
+		template_name='swagger.html',
+		extra_context={'schema_url': 'openapi-schema'}
+	), name='swagger-doc'),
 ]
