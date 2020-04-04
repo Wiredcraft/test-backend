a1f74582304d0aa3a4f2f1504d124d514b40275f
Michael Sayapin <michael@smartmeal.cn>
2020-04-04 16:31:02 +0800

    scaffold django wiredcraft test backend project

---
+++ backend/wiredcraft_test_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for wiredcraft_test_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiredcraft_test_backend.settings')
+
+application = get_asgi_application()
