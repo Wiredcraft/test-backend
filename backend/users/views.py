2cbc59e4968a967efb6a62bde37a238de4c05625
Michael Sayapin <michael@smartmeal.cn>
2020-04-05 11:48:35 +0800

    added audit trail to userdata API

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,13 @@
 
 from django.shortcuts import get_object_or_404
+from django.utils import timezone
 from rest_framework import viewsets
 from rest_framework.response import Response
 
 from users.models import Userdata
 from users.serializers import UserdataSerializer
+
+import logging
 
 
 class UserdataViewSet(viewsets.ModelViewSet):
@@ -32,3 +35,11 @@ class UserdataViewSet(viewsets.ModelViewSet):
 
 	serializer_class = UserdataSerializer
 	queryset = Userdata.objects.all()
+
+	def _audit_trail(self, user=None, action=None, model='default', pk=None):
+		logger = logging.getLogger(f'audit.{model}.{action}')
+		logger.info(f'{timezone.now().isoformat()} {user} {action} {model} pk:{pk}')
+
+	def initial(self, request, *args, **kwargs):
+		self._audit_trail(user=request.user, action=self.action, model='userdata', pk=kwargs.get('pk'))
+		return super().initial(request, *args, **kwargs)
