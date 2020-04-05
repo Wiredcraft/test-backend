af21fb5d6eea7ea805449dc4d368528e448d73c6
Michael Sayapin <michael@smartmeal.cn>
2020-04-05 12:39:44 +0800

    improved logging audit trail

--- backend/utils/mixins.py
+++ backend/utils/mixins.py
@@ -15,12 +15,47 @@ class AuditTrailMixin:
 		logger = logging.getLogger(f'audit.{model}.{action}')
 		logger.info(f'{timezone.now().isoformat()} {user} {action} {model} pk:{pk}')
 
+	def _get_model(self):
+		return getattr(self, 'audit_trail_model', self.get_queryset().model.__name__)
+
+	def _log_change(self, data=None):
+		if not data:
+			return
+		model = self._get_model()
+		# FIXME: should we use pk/id consistently? django's opinion is "pk" but
+		# I keep "id" for compliance with the initial API spec
+		pk = data.get('id')
+		if pk:
+			self._audit_trail(
+				user=self.request.user,
+				action=self.action,
+				model=model,
+				pk=pk,
+			)
+
+	def perform_create(self, serializer):
+		super().perform_create(serializer)
+		self._log_change(serializer.data)
+
+	def perform_update(self, serializer):
+		super().perform_update(serializer)
+		self._log_change(serializer.data)
+
+	def perform_destroy(self, instance):
+		pk = instance.pk
+		super().perform_destroy(instance)
+		self._log_change({'id': pk})
+
 	def initial(self, request, *args, **kwargs):
-		if hasattr(self, 'audit_trail_model'):
-			model = self.audit_trail_model
-		elif self.get_queryset():
-			model = self.get_queryset().model.__name__
-		else:
-			model = 'default'
-		self._audit_trail(user=request.user, action=self.action, model=model, pk=kwargs.get('pk'))
+		# idempotent actions are safe to pre-log
+		# writing actions are logged separately
+		if self.action in ['list', 'retrieve']:
+			model = self._get_model()
+			pk = kwargs.get(self.lookup_url_kwarg or self.lookup_field)
+			self._audit_trail(
+				user=request.user,
+				action=self.action,
+				model=model,
+				pk=pk,
+			)
 		return super().initial(request, *args, **kwargs)
