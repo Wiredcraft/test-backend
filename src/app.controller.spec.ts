e13b2add8da0fd0318d3892a924e87cbeee76624
Cyril Junior <cyril@yu-mi.io>
2020-06-12 00:35:23 +0800

    init: Initialize repo with framework & basic config

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppController } from "./app.controller";
+
+describe( "AppController", () => {
+	let app: TestingModule;
+
+	beforeAll( async () => {
+		app = await Test.createTestingModule( {
+			controllers: [ AppController ]
+		} ).compile();
+	} );
+
+	describe( "getHello", () => {
+		it( "should return \"Hello World!\"", () => {
+			const appController = app.get<AppController>( AppController );
+			expect( appController.getHello() ).toBe( true );
+		} );
+	} );
+} );
---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+
+
+@Module( {
+	imports: [],
+	controllers: [ AppController ]
+} )
+export class AppModule {
+}
