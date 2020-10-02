4b95deb0dec3570bc80e39c1c5960ab8c9f432a9
Jonathan Dai <nightghost5078@gmail.com>
2020-10-02 21:22:56 +0800

    [Model] Fix possible missing key "id" & fix swagger comments

--- src/model/User.ts
+++ src/model/User.ts
@@ -6,11 +6,11 @@
  *     properties:
  *       id:
  *         type: string
- *         description: user id, shall be a string of uuid v4, length limit: 36
+ *         description: user id, shall be a string of uuid v4, length limit 36
  *         example: "29256e52-9ca2-49d5-8b4e-f42578ccae68"
  *       name:
  *         type: string
- *         description: user name, length limit: 32
+ *         description: user name, length limit 32
  *         example: "Jonathan Dai"
  *       dob:
  *         type: integer
@@ -19,11 +19,11 @@
  *         example: 472120968
  *       address:
  *         type: string
- *         description: user address, length limit: 128
+ *         description: user address, length limit 128
  *         example: "Some cool address in city"
  *       description:
  *         type: string
- *         description: user description, length limit: 258
+ *         description: user description, length limit 256
  *         example: "Some cool description of the user"
  *       createdAt:
  *         type: integer
@@ -31,8 +31,9 @@
  *         description: time user data created, shall be a unix timestamp
  *         example: 1579120968
  */
+
 export interface User {
-  id: string;
+  id?: string;
   name: string;
   dob: number;
   address: string;
