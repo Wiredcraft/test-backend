d85c11b676228e925d23fb0fe61ef1f77fc6dc57
Jonathan Dai <nightghost5078@gmail.com>
2020-10-02 21:22:00 +0800

    [Utility] Add Utility

---
+++ src/utility/Utility.ts
@@ -0,0 +1,23 @@
+import * as Joi from "@hapi/joi";
+
+export const genDbKey = (pattern: string, values: { [key: string]: string | number }): string => {
+  let result = pattern;
+  for (const [key, value] of Object.entries(values)) {
+    result = result.replace(key, value as string);
+  }
+  return result;
+};
+
+export const validateWithJoi = async (schema: Joi.AnySchema, data: any): Promise<{ error: Error, value: any }> => {
+  try {
+    return {
+      error: null,
+      value: await schema.validateAsync(data),
+    };
+  } catch (err) {
+    return {
+      error: err,
+      value: null,
+    };
+  }
+};
