8b2548781c11af7fae992b70c132cd3f811dc9d4
Emmanuel N Kyeyune <emmakyeyune@gmail.com>
2020-10-23 22:54:41 +0800

    adds jwt protected and unprotected routes

---
+++ src/routes/protected.ts
@@ -0,0 +1,23 @@
+import { SwaggerRouter } from 'koa-swagger-decorator'
+import { user } from '../controller'
+
+const protectedRouter = new SwaggerRouter()
+
+// USER ROUTES
+protectedRouter.get('/users', user.getUsers)
+protectedRouter.get('/users/:id', user.getUser)
+protectedRouter.post('/users', user.createUser)
+protectedRouter.put('/users/:id', user.updateUser)
+protectedRouter.delete('/users/:id', user.deleteUser)
+
+// Swagger endpoint
+protectedRouter.swagger({
+    title: 'wiredcraft-restful-api',
+    description: 'RESTful API using NodeJS, KOA, Typescript and TypeORM. Middleware uses JWT, CORS, Winston Logger.',
+    version: '1.0.0',
+})
+
+// mapDir will scan the input dir, and automatically call router.map to all Router Class
+protectedRouter.mapDir(__dirname + '/../')
+
+export { protectedRouter }
