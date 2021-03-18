e983904568b26eeb8d16e6b793266d7ca0a443ad
Kengo TODA <skypencil@gmail.com>
2021-03-18 08:02:36 +0800

    feat: enable HTTPS during development phase

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { existsSync, readFileSync } from 'fs';
+
+const CERT_FILE = 'localhost.pem';
+const KEY_FILE = 'localhost-key.pem';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let httpsOptions = {};
+  if (existsSync(`./${KEY_FILE}`) && existsSync(`./${CERT_FILE}`)) {
+    httpsOptions = {
+      key: readFileSync(`./${KEY_FILE}`),
+      cert: readFileSync(`./${CERT_FILE}`),
+    };
+  } else if (process.env.NODE_ENV === 'development') {
+    Logger.log(
+      `Certificate files not found. Put ${KEY_FILE} and ${CERT_FILE} in the project root to use HTTPS.`,
+    );
+  }
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   await app.listen(3000);
 }
 bootstrap();
