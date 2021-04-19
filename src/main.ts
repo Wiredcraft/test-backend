4fd23a28b1680a2022d4775535a62fbc81a14405
Sebastian Scheibe <sebastian@ecostack.nl>
2021-04-19 15:44:49 +0800

    Add API documentation via OpenAPI from Nestjs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('Wiredcraft assignment')
+      .setDescription('Test backend for user, friends and geolocation')
+      .setVersion('1.0')
+      // .addTag('cats')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 
