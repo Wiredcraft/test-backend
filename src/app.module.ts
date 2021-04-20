c6cd379a5ea995bc7997b251a0ce7565b01eb6f0
Sebastian Scheibe <sebastian@ecostack.nl>
2021-04-20 17:33:00 +0800

    Add default route to guide user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './presentation/rest/user/user.module';
 import { PostgresModule } from './infrastructure/postgres/postgres.module';
+import { AppController } from './presentation/rest/app/app.controller';
 
 @Module({
   imports: [UserModule, PostgresModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
---
+++ src/presentation/rest/app/app.controller.ts
@@ -0,0 +1,10 @@
+import {Controller, Get} from "@nestjs/common";
+
+@Controller()
+export class AppController {
+
+    @Get('/')
+    get(){
+        return `Welcome, please check out the API documentation at <a href="/api"/>/api</a>`
+    }
+}
