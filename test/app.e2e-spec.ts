4cc9d1aa50a8bea2022938c7d0ef12c38b7598ce
Sebastian Scheibe <sebastian@ecostack.nl>
2021-04-20 18:30:00 +0800

    Add e2e tests for some error scenarios

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { CustomErrorFilter } from '../src/presentation/rest/custom.error.filter';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -12,6 +13,13 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+    app.useGlobalFilters(new CustomErrorFilter());
     await app.init();
   });
 
@@ -19,6 +27,37 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Welcome, please check out the API documentation at <a href="/api"/>/api</a>');
+      .expect(
+        'Welcome, please check out the API documentation at <a href="/api"/>/api</a>',
+      );
+  });
+
+  it('Add empty user - throw error', () => {
+    return request(app.getHttpServer())
+      .post('/user')
+      .send({ abc: 'abc' })
+      .expect(400)
+      .expect(
+        '{"statusCode":400,"message":["property abc should not exist","name must be a string"],"error":"Bad Request"}',
+      );
+  });
+
+  it('Add user - expect user', () => {
+    return request(app.getHttpServer())
+      .post('/user')
+      .send({ name: 'Philip J. Fry' })
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toHaveProperty('name', 'Philip J. Fry');
+      });
+  });
+
+  it('Add friend with non existing users - throw error', () => {
+    return request(app.getHttpServer())
+      .post('/user/SomeId/friend/SomeOtherId')
+      .expect(400)
+      .then((response) => {
+        expect(response.body).toHaveProperty('message', 'User not found');
+      });
   });
 });
