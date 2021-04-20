56f4102fa988fa19cc6aa1784144a5da50fb01e1
Sebastian Scheibe <sebastian@ecostack.nl>
2021-04-20 18:44:02 +0800

    Add more e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,8 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { CustomErrorFilter } from '../src/presentation/rest/custom.error.filter';
+import { UserEntity } from '../src/infrastructure/postgres/user/user.entity';
+import { FriendEntity } from '../src/infrastructure/postgres/friend/friend.entity';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -21,6 +23,9 @@ describe('AppController (e2e)', () => {
     );
     app.useGlobalFilters(new CustomErrorFilter());
     await app.init();
+
+    await FriendEntity.destroy({ where: {} });
+    await UserEntity.destroy({ where: {} });
   });
 
   it('/ (GET)', () => {
@@ -42,6 +47,16 @@ describe('AppController (e2e)', () => {
       );
   });
 
+  it('Add user with wrong address - expect error', () => {
+    return request(app.getHttpServer())
+      .post('/user')
+      .send({ name: 'Philip J. Fry', address: 'Wrong address format' })
+      .expect(400)
+      .expect(
+        '{"statusCode":400,"message":["address must be an array"],"error":"Bad Request"}',
+      );
+  });
+
   it('Add user - expect user', () => {
     return request(app.getHttpServer())
       .post('/user')
@@ -52,12 +67,109 @@ describe('AppController (e2e)', () => {
       });
   });
 
+  it('Add user and update user - expect user', () => {
+    return request(app.getHttpServer())
+      .post('/user')
+      .send({ name: 'Philip J. Fry' })
+      .expect(201)
+      .then((response) => {
+        const user = response.body;
+        expect(user).toHaveProperty('name', 'Philip J. Fry');
+        expect(user).toHaveProperty('id');
+        return request(app.getHttpServer())
+          .get(`/user/${user.id}`)
+          .expect(200)
+          .then((response) => {
+            const returnedUser = response.body;
+            expect(returnedUser).toEqual(user);
+
+            return request(app.getHttpServer())
+              .patch(`/user/${user.id}`)
+              .send({ dateOfBirth: '2020-04-20' })
+              .expect(200)
+              .then((response) => {
+                return request(app.getHttpServer())
+                  .get(`/user/${user.id}`)
+                  .expect(200)
+                  .then((response) => {
+                    const updatedUser = response.body;
+                    expect(updatedUser).toHaveProperty(
+                      'dateOfBirth',
+                      '2020-04-20',
+                    );
+                  });
+              });
+          });
+      });
+  });
+
   it('Add friend with non existing users - throw error', () => {
     return request(app.getHttpServer())
       .post('/user/SomeId/friend/SomeOtherId')
       .expect(400)
       .then((response) => {
         expect(response.body).toHaveProperty('message', 'User not found');
+      });
+  });
+
+  it('Add two user and make a friend', () => {
+    return request(app.getHttpServer())
+      .post('/user')
+      .send({ name: 'Philip J. Fry 1st' })
+      .expect(201)
+      .then((response) => {
+        const user1 = response.body;
+        return request(app.getHttpServer())
+          .post('/user')
+          .send({ name: 'Philip J. Fry 2nd' })
+          .expect(201)
+          .then((response) => {
+            const user2 = response.body;
+            return request(app.getHttpServer())
+              .post(`/user/${user1.id}/friend/${user2.id}`)
+              .expect(201)
+              .then((response) => {
+                expect(response.body).toHaveProperty('userId', user1.id);
+                expect(response.body).toHaveProperty('otherUserId', user2.id);
+              });
+          });
+      });
+  });
+
+  it('Add two user, make a friend and check nearby', () => {
+    return request(app.getHttpServer())
+      .post('/user')
+      .send({ name: 'Philip J. Fry 1st', address: [31.226133, 121.466505] })
+      .expect(201)
+      .then((response) => {
+        const user1 = response.body;
+        return request(app.getHttpServer())
+          .post('/user')
+          .send({ name: 'Philip J. Fry 2nd', address: [31.225133, 121.465505] })
+          .expect(201)
+          .then((response) => {
+            const user2 = response.body;
+            return request(app.getHttpServer())
+              .post(`/user/${user1.id}/friend/${user2.id}`)
+              .expect(201)
+              .then((response) => {
+                expect(response.body).toHaveProperty('userId', user1.id);
+                expect(response.body).toHaveProperty('otherUserId', user2.id);
+
+                return request(app.getHttpServer())
+                  .get(`/user/${user1.id}/friend/nearby`)
+                  .expect(200)
+                  .then((response) => {
+                    expect(response.body).toHaveLength(1);
+                    expect(response.body[0]).toHaveProperty(
+                      'distance',
+                      146.18796597,
+                    );
+                    expect(response.body[0]).toHaveProperty('id', user2.id);
+                    expect(response.body[0]).toHaveProperty('name', user2.name);
+                  });
+              });
+          });
       });
   });
 });
