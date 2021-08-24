67a67067674ec816fecd45a1f665311ffc162933
Xia Shulin <xiashulin110@qq.com>
2021-08-24 15:50:20 +0800

    remove unnecessary export
    remove unnecessary export

--- db/index.ts
+++ db/index.ts
@@ -2,14 +2,10 @@ import path from 'path';
 import { Sequelize } from 'sequelize-typescript';
 import { MYSQL } from "../config";
 
-const seq = new Sequelize(MYSQL);
-
 export default async function () {
-    // const seq = new Sequelize(MYSQL);
+    const seq = new Sequelize(MYSQL);
 
     seq.addModels([path.resolve(__dirname, './models/')]);
 
     await seq.sync({ force: true });
 }
-
-exports.seq = seq
