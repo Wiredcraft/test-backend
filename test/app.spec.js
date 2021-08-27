16f16e4eae5acbcf6026599c5827d4a4dae5d94d
HaoHaoP <HaoHaoP@HaoHaoP.com>
2021-08-27 17:13:01 +0800

    type: fix scope: test\app.spec.js subject: Used promise to fixed the callback hell problem.Fixed the problem that the database has no data when calling POST or DELETE API.

--- test/app.spec.js
+++ test/app.spec.js
@@ -6,111 +6,121 @@ const app = require('../app');
 chai.should();
 
 chai.use(chaiHttp);
-describe('Access to DB', () => {
-  describe('Access to DB', () => {
-    it('should connected to mongoose', (done) => {
-      app.mongoose.connection.on('connected', () => {
-        done();
-      });
+
+describe('App', () => {
+
+  before((done) => {
+    app.mongoose.connection.on('connected', () => {
+      done();
     });
   });
-  describe('App', () => {
-    it('should list ALL user on /api/user GET', (done) => {
-      chai.request(app)
-        .get('/api/user')
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.should.be.json;
-          res.body.code.should.equal(0);
-          res.body.result.data.should.be.a('array');
-          res.body.result.data[0].should.have.property('_id');
-          res.body.result.data[0].should.have.property('name');
-          res.body.result.data[0].should.have.property('dob');
-          res.body.result.data[0].should.have.property('address');
-          res.body.result.data[0].should.have.property('description');
-          res.body.result.data[0].should.have.property('createdAt');
-          done();
-        });
-    });
 
-    it('should list a SINGLE user on /api/user/:id GET', (done) => {
-      const newUser = new User({
+  it('should list ALL user on /api/user GET', (done) => {
+    chai.request(app)
+      .get('/api/user')
+      .then((res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.code.should.equal(0);
+        res.body.result.data.should.be.a('array');
+        res.body.result.data[0].should.have.property('_id');
+        res.body.result.data[0].should.have.property('name');
+        res.body.result.data[0].should.have.property('dob');
+        res.body.result.data[0].should.have.property('address');
+        res.body.result.data[0].should.have.property('description');
+        res.body.result.data[0].should.have.property('createdAt');
+        done();
+      });
+  });
+
+  it('should get a SINGLE user on /api/user/:id GET', (done) => {
+    const newUser = new User({
+      name: 'HaoHaoP',
+      dob: new Date().valueOf(),
+      address: 'Shanghai',
+      description: 'In Shanghai',
+      createdAt: new Date().valueOf(),
+    });
+    newUser.save()
+      .then(data => {
+        return chai.request(app).get('/api/user/' + data._id)
+      })
+      .then((res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.code.should.equal(0);
+        res.body.result.data.should.be.a('array');
+        res.body.result.data[0].should.have.property('_id');
+        res.body.result.data[0].should.have.property('name');
+        res.body.result.data[0].should.have.property('dob');
+        res.body.result.data[0].should.have.property('address');
+        res.body.result.data[0].should.have.property('description');
+        res.body.result.data[0].should.have.property('createdAt');
+        done();
+      });
+  });
+
+  it('should add a SINGLE user on /api/user POST', (done) => {
+    chai.request(app)
+      .post('/api/user')
+      .send({
         name: 'HaoHaoP',
         dob: new Date().valueOf(),
         address: 'Shanghai',
         description: 'In Shanghai',
-        createdAt: new Date().valueOf(),
+      })
+      .then((res) => {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.code.should.equal(0);
+        done();
       });
-      newUser.save(function(err, data) {
-        chai.request(app)
-          .get('/api/user/' + data._id)
-          .end((error, response) => {
-            response.should.have.status(200);
-            response.should.be.json;
-            response.body.code.should.equal(0);
-            response.body.result.data.should.be.a('array');
-            response.body.result.data[0].should.have.property('_id');
-            response.body.result.data[0].should.have.property('name');
-            response.body.result.data[0].should.have.property('dob');
-            response.body.result.data[0].should.have.property('address');
-            response.body.result.data[0].should.have.property('description');
-            response.body.result.data[0].should.have.property('createdAt');
-            done();
+  });
+
+  it('should delete a SINGLE user on /api/user/:id DELETE', (done) => {
+    const newUser = new User({
+      name: 'HaoHaoP',
+      dob: new Date().valueOf(),
+      address: 'Shanghai',
+      description: 'In Shanghai',
+      createdAt: new Date().valueOf(),
+    });
+    newUser.save()
+      .then(data => {
+        return chai.request(app).delete('/api/user/' + data._id)
+      })
+      .then((res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.code.should.equal(0);
+        done();
+      });
+  });
+
+  it('should update a SINGLE user on /api/user/:id PUT', (done) => {
+    const newUser = new User({
+      name: 'HaoHaoP',
+      dob: new Date().valueOf(),
+      address: 'Shanghai',
+      description: 'In Shanghai',
+      createdAt: new Date().valueOf(),
+    });
+    newUser.save()
+      .then(data => {
+        return chai.request(app)
+          .put('/api/user/' + data._id)
+          .send({
+            name: 'Edit HaoHaoP',
+            dob: new Date().valueOf(),
+            address: 'Edit Shanghai',
+            description: 'Edit In Shanghai',
           });
+      })
+      .then((res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.code.should.equal(0);
+        done();
       });
-    });
-
-    it('should add a SINGLE user on /api/user PUT', (done) => {
-      chai.request(app)
-        .post('/api/user')
-        .send({
-          name: 'HaoHaoP',
-          dob: new Date().valueOf(),
-          address: 'Shanghai',
-          description: 'In Shanghai',
-        })
-        .end((err, res) => {
-          res.should.have.status(201);
-          res.should.be.json;
-          res.body.code.should.equal(0);
-          done();
-        });
-    });
-
-    it('should delete a SINGLE user on /api/user/:id DELETE', (done) => {
-      chai.request(app)
-        .get('/api/user')
-        .end((err, res) => {
-          chai.request(app)
-            .delete('/api/user/' + res.body.result.data[res.body.result.data.length - 1]._id)
-            .end((error, response) => {
-              response.should.have.status(200);
-              response.should.be.json;
-              response.body.code.should.equal(0);
-              done();
-            });
-        });
-    });
-
-    it('should update a SINGLE user on /api/user/:id POST', (done) => {
-      chai.request(app)
-        .get('/api/user')
-        .end((err, res) => {
-          chai.request(app)
-            .put('/api/user/' + res.body.result.data[res.body.result.data.length - 1]._id)
-            .send({
-              name: 'HaoHaoP',
-              dob: new Date().valueOf(),
-              address: 'Shanghai',
-              description: 'In Shanghai',
-            })
-            .end((error, response) => {
-              response.should.have.status(200);
-              response.should.be.json;
-              response.body.code.should.equal(0);
-              done();
-            });
-        });
-    });
   });
 });
