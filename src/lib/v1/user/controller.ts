f24cc14ba870968a1334129b9aabc5f5f140f6f7
fawzi bedidi <Fawzi.bedidi@icloud.com>
2022-03-25 18:06:36 +0800

    feature: Init routes

---
+++ src/lib/v1/user/controller.ts
@@ -0,0 +1,11 @@
+import Koa from "koa";
+
+export const createUser = async (ctx: Koa.Context): Promise<void> => {}
+
+export const deleteUser = async (ctx: Koa.Context): Promise<void> => {}
+
+export const patchUser = async (ctx: Koa.Context): Promise<void> => {}
+
+export const updateUser = async (ctx: Koa.Context): Promise<void> => {}
+
+export const getUser = async (ctx: Koa.Context): Promise<void> => {}
---
+++ src/lib/v1/user/router.ts
@@ -0,0 +1,13 @@
+import Router from 'koa-router'
+import * as controller from "./controller"
+const router = new Router()
+
+router.post('Create User', '/user', controller.createUser)
+router.delete('Delete User', '/user/:userId', controller.deleteUser)
+router.patch('Patch User', '/user/:userId', controller.patchUser)
+router.put('Update User', '/user/:userId', controller.updateUser)
+router.get('Get User', '/user/:userId', controller.getUser)
+
+router.routes()
+
+export { router }
