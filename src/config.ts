903bb20510ff92649d554d910d7cd1a848403d2f
fawzi bedidi <Fawzi.bedidi@icloud.com>
2022-03-28 11:05:47 +0800

    feature: Start

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,13 @@ export const config = {
     appName: process.env.APP_NAME || "Wiredcraft-test",
     port: process.env.PORT || 3000,
     env: process.env.NODE_ENV || 'development',
-    version: require("../package.json").version
+    version: require("../package.json").version,
+    database: {
+        mongo: {
+            url: process.env.MONGO_URL || "mongodb://localhost:27017",
+            name: process.env.DATABASE_NAME || "test"
+        },
+    },
 } as const
 
 export const isDevelopment = config.env === 'development'
