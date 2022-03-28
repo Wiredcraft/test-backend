dbc672b2dbfd7ea5a47171755fe8b2155469d0f3
fawzi bedidi <Fawzi.bedidi@icloud.com>
2022-03-28 14:34:20 +0800

    feature: Update tests and add fixtures

--- src/lib/v1/user/__tests__/user.test.ts
+++ src/lib/v1/user/__tests__/user.test.ts
@@ -1,39 +1,51 @@
 import supertest from "supertest"
+
 import {app} from "../../../../app"
 import {UserModel} from "../model";
-import dayjs from "dayjs";
 import {createRouteParams, patchRouteParams, updateRouteParams} from "../validator";
-import {string} from "joi";
-
+import * as db from "../../../utils/mongoDb"
+import * as fixtures from "./fixtures"
 
 const server = app.callback()
 const request: supertest.SuperTest<supertest.Test> = supertest(server)
 
+jest.setTimeout(50000)
 
-beforeAll(() => {
-    // TODO create fixtures here
+beforeAll(async () => {
+    await db.waitForConnection()
+    await db.mongoose.connection.db.dropDatabase()
+
+    await db.mongoose.connection.db.collection('users').insertMany(fixtures.users)
 })
 
-afterAll(() => {
-    // TODO remove fixtures here
+afterAll(async () => {
+    await db.mongoose.connection.db.dropDatabase()
+    await db.mongoose.connection.close(true)
 })
+
 
 describe("/user routes", () => {
 
-    it("should return 200 and the user profile", async () => {
-        const userId = "FAKE_USER_ID"
+    it("should return 404 when getting a user that does not exist", async () => {
         const res = await request
-            .get(`/v1/user/${userId}`)
+            .get(`/v1/user/THIS_USER_DOES_NOT_EXIST`)
+            .send()
+
+        expect(res.statusCode).toEqual(404)
+    })
+
+    it("should return 200 and the user profile", async () => {
+        const res = await request
+            .get(`/v1/user/${fixtures.users[0].id}`)
             .send()
 
         expect(res.statusCode).toEqual(200)
         const user = JSON.parse(res.text) as UserModel
 
-        expect(typeof user.name).toBe('string')
-        expect(typeof user.dateOfBirth).toBe('string')
-        expect(dayjs(user.dateOfBirth).isBefore(dayjs())).toBe(true)
-        expect(typeof user.description).toBe('string')
-        expect(typeof user.address).toBe('string')
+        expect(user.name).toBe(fixtures.users[0].name)
+        expect(user.dob).toBe(fixtures.users[0].dob)
+        expect(user.description).toBe(fixtures.users[0].description)
+        expect(user.address).toBe(fixtures.users[0].address)
     })
 
     it("should return 200 and all user profiles", async () => {
@@ -43,80 +55,132 @@ describe("/user routes", () => {
 
         expect(res.statusCode).toEqual(200)
         const body = JSON.parse(res.text) as UserModel[]
-        expect(body.length).toBeGreaterThan(0)
+        expect(body.length).toBe(fixtures.users.length)
 
-        for (const user of body) {
-            expect(typeof user.name).toBe('string')
-            expect(typeof user.dateOfBirth).toBe('string')
-            expect(dayjs(user.dateOfBirth).isBefore(dayjs())).toBe(true)
-            expect(typeof user.description).toBe('string')
-            expect(typeof user.address).toBe('string')
+        for (let i = 0; i < fixtures.users.length; i++) {
+            expect(body[i].name).toBe(fixtures.users[i].name)
+            expect(body[i].dob).toBe(fixtures.users[i].dob)
+            expect(body[i].description).toBe(fixtures.users[i].description)
+            expect(body[i].address).toBe(fixtures.users[i].address)
         }
     })
 
     it("should return 200 and create a user profile", async () => {
         const body: createRouteParams = {
             name: "Fake User",
-            dateOfBirth: "06-14-1994",
+            dob: "06-14-1994",
             address: "This is an address",
             description: "This is a description",
         }
-        const res = await request
+        let res = await request
             .post(`/v1/user`)
             .send(body)
 
         expect(res.statusCode).toEqual(200)
-        const response = JSON.parse(res.text) as UserModel
-        expect(response.dateOfBirth).toBe(body.dateOfBirth)
+
+        // Check response value
+        let response = JSON.parse(res.text) as UserModel
+        expect(response.dob).toBe(body.dob)
         expect(response.id).toBeDefined()
+        expect(response.name).toBe(body.name)
+        expect(response.address).toBe(body.address)
+        expect(response.description).toBe(body.description)
+
+        const id = response.id
+        // Check that we can the GET endpoint returns it correctly
+        res = await request
+            .get(`/v1/user/${id}`)
+            .send()
+
+        response = JSON.parse(res.text) as UserModel
+        expect(response.dob).toBe(body.dob)
         expect(response.name).toBe(body.name)
         expect(response.address).toBe(body.address)
         expect(response.description).toBe(body.description)
     })
 
     it("should return 200 and update a user profile", async () => {
-        const body: updateRouteParams = {
-            name: "Fake User",
-            dateOfBirth: "06-14-1994",
+        const postBody: patchRouteParams = {
+            name: "User To Be Deleted",
+            dob: "06-14-1994",
             address: "This is an address",
             description: "This is a description",
         }
-        const res = await request
-            .put(`/v1/user/FAKE_USER_ID`)
+        let res = await request
+            .post(`/v1/user`)
+            .send(postBody)
+        let response = JSON.parse(res.text) as UserModel
+
+        const body: updateRouteParams = {
+            name: "Fake User",
+            dob: "06-14-1994",
+            address: "This is an address",
+            description: "This is a description",
+        }
+        res = await request
+            .put(`/v1/user/${response.id}`)
             .send(body)
 
         expect(res.statusCode).toEqual(200)
-        const response = JSON.parse(res.text) as UserModel
-        expect(response.dateOfBirth).toBe(body.dateOfBirth)
+        response = JSON.parse(res.text) as UserModel
+        expect(response.dob).toBe(body.dob)
         expect(response.name).toBe(body.name)
         expect(response.address).toBe(body.address)
         expect(response.description).toBe(body.description)
+    })
+
+
+    it("should return 200 and patch a user profile by updating only one field", async () => {
+        const postBody: patchRouteParams = {
+            name: "User To Be Deleted",
+            dob: "06-14-1994",
+            address: "This is an address",
+            description: "This is a description",
+        }
+        let res = await request
+            .post(`/v1/user`)
+            .send(postBody)
+        let response = JSON.parse(res.text) as UserModel
+
+        const body: patchRouteParams = {
+            address: "This is another address",
+        }
+        res = await request
+            .patch(`/v1/user/${response.id}`)
+            .send(body)
+
+        expect(res.statusCode).toEqual(200)
+        response = JSON.parse(res.text) as UserModel
+        expect(response.dob).toBe(postBody.dob)
+        expect(response.name).toBe(postBody.name)
+        expect(response.address).toBe(body.address)
+        expect(response.description).toBe(postBody.description)
     })
 
     it("should return 200 and delete a user profile", async () => {
-        const res = await request
-            .del(`/v1/user/FAKE_USER_ID`)
-            .send()
 
-        expect(res.statusCode).toEqual(200)
-    })
-
-    it("should return 200 and patch a user profile", async () => {
         const body: patchRouteParams = {
-            name: "Fake User",
-            dateOfBirth: "06-14-1994",
+            name: "User To Be Deleted",
+            dob: "06-14-1994",
             address: "This is an address",
             description: "This is a description",
         }
-        const res = await request
-            .patch(`/v1/user/FAKE_USER_ID`)
+        let res = await request
+            .post(`/v1/user`)
             .send(body)
+        const response = JSON.parse(res.text) as UserModel
+
+
+        res = await request
+            .del(`/v1/user/${response.id}`)
+            .send()
 
         expect(res.statusCode).toEqual(200)
-        const response = JSON.parse(res.text) as UserModel
-        expect(response.dateOfBirth).toBe(body.dateOfBirth)
-        expect(response.name).toBe(body.name)
-        expect(response.address).toBe(body.address)
-        expect(response.description).toBe(body.description)
+
+        res = await request
+            .get(`/v1/user/${response.id}`)
+            .send()
+
+        expect(res.statusCode).toEqual(404)
     })
 })
