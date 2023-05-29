fbd6ea8d9cf9cbc570bbde6b82609cb12ccdf4d7
Yingjun Cui <yingjuncui@gmail.com>
2023-05-29 18:36:42 +0800

    clean up strip logic, make it resuable

--- src/pipes/request-strip.pipe.ts
+++ src/pipes/request-strip.pipe.ts
@@ -1,12 +1,20 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common";
+import UserDto from "@wiredcraft/users/dto/user.dto";
 
 @Injectable()
 export class RequestStripPipe implements PipeTransform {
+  private readonly excludedFields: string[] = ["id", "createAt", "updateAt"];
+
   transform(value: any, metadata: ArgumentMetadata) {
     if (metadata?.metatype?.name === "UserDto") {
-      delete value.id;
-      delete value.createAt;
-      delete value.updateAt;
+      return this.excludeFields(value);
+    }
+    return value;
+  }
+  private excludeFields(value: UserDto) {
+    if (null === value || typeof value !== "object") return value;
+    for (const key of this.excludedFields) {
+      delete value[key];
     }
     return value;
   }
