ef8026ce4febcb01f1e92e2d0972fa216df0f208
MuriuDaudi <daudi@sendyit.com>
2023-10-10 01:03:36 +0300

    Create users model and map the columns on the db
    Signed-off-by: MuriuDaudi <daudi@sendyit.com>

---
+++ app/Models/User.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public date_of_birth: DateTime
+
+  @column()
+  public address: String
+
+  @column()
+  public description: String
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
