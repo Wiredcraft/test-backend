60b143b56d90c740bb3634746526c61e883a3f26
MuriuDaudi <daudi@sendyit.com>
2023-10-10 00:58:08 +0300

    create database migration schema for Users table
    Signed-off-by: MuriuDaudi <daudi@sendyit.com>

---
+++ database/migrations/1696888203583_users.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').comment('The users name')
+      table.date('date_of_birth').comment('The users date of birth')
+      table.json('address').comment('Stores address information for the user as a JSON object')
+      table.text('description')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
