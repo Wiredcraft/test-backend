5f4eea05d501f689f5c4337f8f85b9f2ba37b389
MuriuDaudi <daudi@sendyit.com>
2023-10-10 02:18:02 +0300

    Add delete and patch routes for the users resource
    Signed-off-by: MuriuDaudi <daudi@sendyit.com>

--- start/routes/users.ts
+++ start/routes/users.ts
@@ -4,4 +4,10 @@ Route.group(() => {
   Route.get('', 'UsersController.index') //Lists users
 
   Route.post('', 'UsersController.store') //Store user
+
+  Route.group(() => {
+    Route.patch('', 'UsersController.update') //Update user
+
+    Route.delete('', 'UsersController.destroy') //Delete user
+  }).prefix(':userID')
 }).prefix('/api/v1/users')
