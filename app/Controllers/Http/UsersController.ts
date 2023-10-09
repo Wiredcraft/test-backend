5dc15b8b652bf99d3ba6431f6bad8f3f7abb1732
MuriuDaudi <daudi@sendyit.com>
2023-10-10 01:05:38 +0300

    Create a resourceful controller to handl the CRUD operations to/on a user
    Signed-off-by: MuriuDaudi <daudi@sendyit.com>

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,13 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UsersController {
+  public async index({}: HttpContextContract) {}
+
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
