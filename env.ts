e3a46ec6ac9a9fee580d9d513d44241dd9a6b5a8
MuriuDaudi <daudi@sendyit.com>
2023-10-10 00:49:28 +0300

    configure lucid ORM
    Signed-off-by: MuriuDaudi <daudi@sendyit.com>

--- env.ts
+++ env.ts
@@ -21,4 +21,19 @@ export default Env.rules({
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+
+  /**
+   * Variables for the MYSQL driver
+   */
+
+  MYSQL_HOST: Env.schema.string({ format: 'host' }),
+  MYSQL_PORT: Env.schema.number(),
+  MYSQL_USER: Env.schema.string(),
+  MYSQL_PASSWORD: Env.schema.string.optional(),
+  MYSQL_DB_NAME: Env.schema.string(),
+
+  // * The MYSQL_HOST should always be present and formatted as a valid host.
+  // * The MYSQL_PORT should always be present and a valid number.
+  // * The MYSQL_USER and MYSQL_PASSWORD are required to authenticate with the database server. The password is marked as optional since many local database servers are configured to work without passwords.
+  // * The MYSQL_DB_NAME is the database name you want to connect with.
 })
